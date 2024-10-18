179443e28f24a9b073e08ac16b12ad5275c7cd1b
Mohit Kumar <114150173+MKD2002@users.noreply.github.com>
2024-10-18 10:35:59 +0530

    Update index.js 2

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const corsOptions = {
 app.use(cors(corsOptions))
 app.options('*', cors(corsOptions));
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');  // Allow all origins
+    res.header('Access-Control-Allow-Origin', process.env.ORIGIN);  // Allow all origins
     res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
