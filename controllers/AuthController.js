a94a57f1f43183109251038da9b763982f5b70d6
Mohit Kumar <114150173+MKD2002@users.noreply.github.com>
2024-10-21 15:03:26 +0530

    Update AuthController.js

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -18,7 +18,6 @@ export const signup = async(request,response,next) => {
         const user = await User.create({email,password});
         response.cookie("jwt",createToken(email,user.id),{
             maxAge,
-            secure: true,
             sameSite: "None",
         });
         return response.status(201).json({
@@ -51,7 +50,6 @@ export const login = async(request,response,next) => {
 
         response.cookie("jwt",createToken(email,user.id),{
             maxAge,
-            secure: true,
             sameSite: "None",
         });
         return response.status(200).json({
