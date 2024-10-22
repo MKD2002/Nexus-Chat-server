5b2461eeddc65ee4bbea58a8d7de9bebcf6e4759
MKD2002 <mohitkd4444@gmail.com>
2024-10-22 16:22:54 +0530

    added some allowedHeader

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const corsOptions = {
     origin:process.env.ORIGIN,
     methods:['GET','POST','PUT','DELETE','PATCH'],
     credentials:true,
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    allowedHeaders: ['Content-Type', 'Authorization','application/json','image/*'],
     exposedHeaders: '*',
 };
 
